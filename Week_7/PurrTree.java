bc8e34f2c0a33d6a8f7c7fd49a9fdeac2d92034c
Purrbiscuit <spurr@articulate.com>
2020-07-16 20:47:22 -0400

    add leafCount methods to Week_7

--- Week_7/PurrTree.java
+++ Week_7/PurrTree.java
@@ -1,15 +1,21 @@
+import ch02.stacks.*;
 import ch07.trees.*;
 import support.BSTNode;
 
 public class PurrTree<T> extends BinarySearchTree<T>
 {
+  //---------------------------------------
+  //
   // Problem 29 - min2 and recMin2 methods
+  //
+  //---------------------------------------
+
   public T min2()
   // entrypoint to recursive recMin2 method
   {
     return recMin2(root);
   };
-  
+
   public T recMin2(BSTNode<T> node)
   // recursive method to get the minimum value in the tree
   {
@@ -17,5 +23,50 @@ public class PurrTree<T> extends BinarySearchTree<T>
       return null;
       
     return node.getLeft() != null ? recMin2(node.getLeft()) : node.getInfo();
+  };
+
+  //---------------------------------------
+  //
+  // Problem 30 -  leafCount methods
+  //
+  //---------------------------------------
+  public int leafCount()
+  {
+    int count = 0;
+    if (root != null)
+    {
+      LinkedStack<BSTNode<T>> nodeStack = new LinkedStack<BSTNode<T>>();
+      BSTNode<T> currNode;
+      nodeStack.push(root);
+
+      while (!nodeStack.isEmpty())
+      {
+        currNode = nodeStack.top();
+        nodeStack.pop();
+        if (currNode.getLeft() != null)
+          nodeStack.push(currNode.getLeft());
+        if (currNode.getRight() != null)
+          nodeStack.push(currNode.getRight());
+
+        if (currNode.getRight() == null && currNode.getLeft() == null)
+          count++;
+      }
+    }
+    return count;
+  };
+
+  public int leafCount2()
+  {
+    return recLeafCount(root);
+  };
+
+  private int recLeafCount(BSTNode<T> node)
+  {
+    if (node == null)
+      return 0;
+    if (node.getLeft() == null && node.getRight() == null)
+      return 1;
+    else
+      return recLeafCount(node.getLeft()) + recLeafCount(node.getRight());
   };
 }
--- Week_7/PurrTreeDriver.java
+++ Week_7/PurrTreeDriver.java
@@ -2,9 +2,6 @@ import java.util.*;
 
 public class PurrTreeDriver
 {
-  // instantiate the PurrTree object
-  private static PurrTree<Integer> purrTree = new PurrTree<>();
-    
   public static void main(String[] args)
   {
     //----------------------------------
@@ -12,17 +9,67 @@ public class PurrTreeDriver
     // Portion of Driver for Problem 29
     //
     //-----------------------------------
+    boxyPrinter("Problem 29 (Min Value) Output:");
 
-    boxyPrinter("Problem 29 Output:");
-    
+    // instantiate a tree1 object
+    PurrTree<Integer> tree1 = new PurrTree<>();
+
     // add 12 random numbers between 1 and 100 to the tree
-    randomAdd(12, 100, true);
-    
+    randomAdd(tree1, 12, 100, true);
+
     // find the min value in the tree using the recursive min2 method
-    System.out.println("\nMinimum value in tree is: " + purrTree.min2());
+    System.out.println("\nMinimum value in tree1 is: " + tree1.min2() + "\n");
+
+    //----------------------------------
+    //
+    // Portion of Driver for Problem 30
+    //
+    //-----------------------------------
+    boxyPrinter("Problem 30 (Leaf Count) Output:");
+
+    // instantiate a tree2 object
+    PurrTree<Integer> tree2 = new PurrTree<>();
+
+    // check leafCount on tree with no nodes
+    System.out.println("Iterative leaf count on tree2 with no nodes: " + tree2.leafCount());
+    System.out.println("Recursive leaf count on tree2 with no nodes: " + tree2.leafCount2() + "\n");
+
+    // check leafCount on tree with one node
+    tree2.add(100);
+    System.out.println("Iterative leaf count on tree2 with one node: " + tree2.leafCount());
+    System.out.println("Recursive leaf count on tree2 with one node: " + tree2.leafCount2() + "\n");
+
+    // reassign tree2 to new PurrTree object and add values
+    // in specific order to tree2 to control leaf count
+    tree2 = new PurrTree<>();
+    orderedAdd(tree2);
+
+    // check iterative leaf count again in tree2 and make
+    // sure it's equal to 5 based on insertion order above
+    int itLeafTotal = tree2.leafCount();
+
+    if (itLeafTotal == 5)
+      System.out.println("Iterative leaf count on tree2 after ordered insertions: " + itLeafTotal);
+    else
+      System.out.println("[ERROR] - Something went wrong; iterative leaf count is not 5 - total is: " + itLeafTotal);
+
+    // check recursive leaf count again in tree2 and make
+    // sure it's equal to 5 based on insertion order above
+    int recLeafTotal = tree2.leafCount2();
+
+    if (recLeafTotal == 5)
+      System.out.println("Recursive leaf count on tree2 after ordered insertions: " + recLeafTotal + "\n");
+    else
+      System.out.println("[ERROR] - Something went wrong; recursive leaf count is not 5 - total is: " + recLeafTotal + "\n");
+
+    // final check - add a bunch of new random value and check leaf count afterward
+    // add 100 more random numbers between 1 and 100 to the tree
+    randomAdd(tree2, 100, 100, false);
+    System.out.println("Iterative leaf count on tree2 after random insertions: " + tree2.leafCount());
+    System.out.println("Recursive leaf count on tree2 after random insertions: " + tree2.leafCount2() + "\n");
   }
   
-  private static void randomAdd(int additions, int maxValue, boolean logOut)
+  private static void randomAdd(PurrTree<Integer> tree, int additions, int maxValue, boolean logOut)
   // helper method to add random integers to the tree
   {
     Random r = new Random();
@@ -30,13 +77,23 @@ public class PurrTreeDriver
     for (int i = 1; i <= additions; i++)
     {
       int n = r.nextInt(maxValue - 1) + 1;
-      purrTree.add(n);
+      tree.add(n);
       
       if (logOut)
         System.out.println("Adding -> " + n);
     };
   };
   
+  private static void orderedAdd(PurrTree<Integer> tree)
+  // helper method to insert items into a tree in a controlled
+  // fashion so the height is 5 and the leaf count is 5
+  {
+    int[] nums = {100, 60, 150, 40, 80, 120, 200, 20, 50, 10, 55, 5};
+
+    for (int i = 0; i < nums.length; i++)
+      tree.add(nums[i]);
+  };
+
   private static void boxyPrinter(String text)
   // helper method to print text to the console in a boxed format
   {
