b49f3dd43d11dbe962421b189558911570c32708
Purrbiscuit <spurr@articulate.com>
2020-07-26 18:49:06 -0400

    add comments to the BSTMap methods

--- Week_8/BSTMap.java
+++ Week_8/BSTMap.java
@@ -3,25 +3,25 @@ import java.util.Iterator;
 
 public class BSTMap<K, V> implements MapInterface<K, V>
 {
-  // declare a BinarySearchTree object to wrap this class around
+  // declare a BinarySearchTree object to wrap this class around.
   private BinarySearchTree<MapEntry<K, V>> map;
   
   public BSTMap()
-  // constructor which instantiates the wrapper BinarySearchTree object
+  // constructor which instantiates the wrapper BinarySearchTree object.
   {
     map = new BinarySearchTree<MapEntry<K, V>>();
   }
   
   public boolean contains(K k)
-  // returns true if key is found in map otherwise returns false
-  // wrapper around BinarySearchTree contains method
+  // returns true if key is found in map otherwise returns false.
+  // wrapper around BinarySearchTree contains method.
   {
     return map.contains(new MapEntry<K, V>(k, null));
   }
   
   public V get(K k)
   // looks for a key in the map and returns the value if found
-  // otherwise returns null; wrapper around BinarySearchTree get method
+  // otherwise returns null.  wrapper around BinarySearchTree get method.
   {
     if (k == null)
       throw new IllegalArgumentException("Maps do not allow null keys.");
@@ -32,15 +32,17 @@ public class BSTMap<K, V> implements MapInterface<K, V>
   }
   
   public boolean isEmpty()
-  // return true if no keys in map; otherwise returns false
-  // wrapper around BinarySearchTree isEmpty method
+  // return true if no keys in map; otherwise returns false.
+  // wrapper around BinarySearchTree isEmpty method.
   {
     return map.isEmpty();
   }
   
   public boolean isFull()
+  // always returns false since a map can never be full.
+  // wrapper around BinarySearchTree isFull method.
   {
-    return false;
+    return map.isFull();
   }
   
   public Iterator<MapEntry<K, V>> iterator()
@@ -50,6 +52,9 @@ public class BSTMap<K, V> implements MapInterface<K, V>
   }
   
   public V put(K k, V v)
+  // adds a key-value pair to the map; will return the old value at
+  // a key if found and replace the old key-value; otherwise will return null.
+  // wrapper around BinarySearchTree add method.
   {
     if (k == null)
       throw new IllegalArgumentException("Maps do not allow null keys.");
@@ -65,6 +70,9 @@ public class BSTMap<K, V> implements MapInterface<K, V>
   }
   
   public V remove(K k)
+  // removes a key-value pair from the map if found
+  // and returns the old value; otherwise returns null.
+  // wrapper around BinarySearchTree remove method.
   {
     if (k == null)
       throw new IllegalArgumentException("Maps do not allow null keys.");
@@ -78,6 +86,7 @@ public class BSTMap<K, V> implements MapInterface<K, V>
   }
   
   public int size()
+  // wrapper around BinarySearchTree size method.
   {
     return map.size();
   }
