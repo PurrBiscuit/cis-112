c9ecb332a93f70ea46464d957fe030e59478ad1a
Michael Karpinski <mike.karpinski99@gmail.com>
2020-08-01 18:56:15 -0400

    Start of lab 2: created Main method that utilizes JOptionPane to take and validate user input

---
+++ Lab_2/Main.java
@@ -0,0 +1,49 @@
+import javax.swing.JOptionPane;
+
+public class Main
+{
+   public static String genericPrompt = "Enter a name and quantity for order separated by a colon.\n" +
+                                        "Example: 'Michael: 3'.\n" +
+                                        "Enter STOP to exit program.";
+   
+   public static void main(String[] args)
+   {
+      takeOrder(false);
+   }
+   
+   public static void parseOrder(String input)
+   // Validates and parses the order string.
+   {
+      boolean invalid = false;
+      
+      if (input.matches("([A-Z])\\w+:([0-9])+"))
+      {
+         String name = input.split(":")[0];
+         int numTickets = Integer.parseInt(input.split(":")[1]);
+         placeOrder(name, numTickets);
+      }
+      else
+         invalid = true;
+      
+      takeOrder(invalid);
+   }
+   
+   public static void placeOrder(String name, int numTickets)
+   // Eventually, this will call the second thread.
+   {
+      System.out.println(name + ": " + numTickets);
+   }
+   
+   public static void takeOrder(boolean previousInvalid)
+   // Takes order string and passes to parseOrder method.
+   {
+      String prompt = genericPrompt;
+      
+      if (previousInvalid)
+         prompt = "Invalid entry. Please try again.\n" + prompt;
+      
+      String input = JOptionPane.showInputDialog(prompt).replaceAll(" ", "");
+      
+      parseOrder(input);
+   }
+}
