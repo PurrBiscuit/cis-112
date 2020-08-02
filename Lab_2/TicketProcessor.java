6e76068d2377566b32bd8fcc9b4db70ebf339737
Purrbiscuit <spurr@articulate.com>
2020-08-01 21:38:51 -0400

    use volatile flag to stop TicketProcessor thread instead

--- Lab_2/TicketProcessor.java
+++ Lab_2/TicketProcessor.java
@@ -6,45 +6,50 @@ class TicketProcessor extends Thread
    private static int timeoutPeriod = 5000;
    int ticketsAvailable = 10;
    LinkedQueue queue;
+   private volatile boolean stop = false;
    
    public void addOrder(Order order)
    {
       queue.enqueue(order);
    }
    
+   public void end()
+   {
+      stop = true;
+   }
+
    public void checkTickets()
    // Checks whether any orders are in the queue. If so, process all orders.
    {
-      try { Thread.sleep(timeoutPeriod); } catch (Exception e) {}
+      while (!stop)
+      {
+         try { Thread.sleep(timeoutPeriod); } catch (Exception e) {}
 
-      if (queue.isEmpty())
-         System.out.println("No orders to process.");
-      else
-         processOrders();
-      
-      System.out.println(ticketsAvailable + " tickets remaining.");
-      checkTickets();
+         if (queue.isEmpty())
+            System.out.println("No orders to process.");
+         else
+            processOrders();
+
+         System.out.println(ticketsAvailable + " tickets remaining.");
+         checkTickets();
+      }
    }
    
    public void processOrders()
    // Goes through all orders in queue, determines whether order is valid, removes appropriate number of tickets, and prints order details.
    // CURRENTLY NOT WORKING.
    {
-      System.out.println(queue.dequeue());
-      while (!queue.isEmpty())
+      Order order = (Order)queue.dequeue();
+
+      if ((ticketsAvailable - order.numTickets) >= 0)
       {
-         Order order = (Order)queue.dequeue();
-         order.setId("[PLACEHOLDER]");
-         if ((ticketsAvailable - order.numTickets) >= 0)
-         {
-            ticketsAvailable -= order.numTickets;
-            order.setStatus(true);
-         }
-         order.setProcessedTime("[PLACEHOLDER]");
-         
-         System.out.println("\nORDER PROCESSED");
-         System.out.println(order + "\n\n\n");
+         ticketsAvailable -= order.numTickets;
+         order.setStatus(true);
       }
+      order.setProcessedTime("[PLACEHOLDER]");
+
+      System.out.println("\nORDER PROCESSED");
+      System.out.println(order + "\n\n\n");
    }
    
    public void run()
