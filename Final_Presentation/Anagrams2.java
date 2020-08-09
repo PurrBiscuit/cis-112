70f582ce57e7080adbf87982572a7d876c1c1fff
Purrbiscuit <spurr@articulate.com>
2020-08-08 20:04:32 -0400

    complete Anagrams2 grouping algorithm

--- Final_Presentation/Anagrams2.java
+++ Final_Presentation/Anagrams2.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Anagrams2 extends AnagramHelper
 {
    public static void main(String[] args)
@@ -7,5 +11,44 @@ public class Anagrams2 extends AnagramHelper
       // print out the ungrouped array content first for comparison
       boxyPrinter("Ungrouped Array output:");
       prettyPrint(words);
+
+      // print out the grouped array after running through sort method
+      boxyPrinter("Grouped Map output:");
+      sort(words);
+      prettyPrint(words);
+   }
+
+   private static void sort(String[] a)
+   {
+      HashMap<String, ArrayList<String>> map = new HashMap<>();
+
+      // group words by anagram into map
+      for (String s : a)
+      {
+         String key = sortChars(s);
+         if (!map.containsKey(key)) {
+            map.put(key, new ArrayList<String>());
+         }
+
+         map.get(key).add(s);
+      }
+
+      // add map values back to array in correct groupings
+      int i = 0;
+      for (String key : map.keySet())
+      {
+         ArrayList<String> list = map.get(key);
+         for (String s : list)
+         {
+            a[i] = s;
+            i++;
+         }
+      }
+   }
+
+   private static String sortChars(String s) {
+      char[] content = s.toCharArray();
+      Arrays.sort(content);
+      return new String(content);
    }
 }
